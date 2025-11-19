17c410ec1c3f7f32e7ab06f996cab9a5297b2338
RuslanName <ruslangeroy@gmail.com>
2025-11-20 00:01:26 +0300

    SSL fix, again 2

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 if ! command -v envsubst > /dev/null 2>&1; then
     apk add --no-cache gettext
@@ -7,42 +6,71 @@ fi
 
 if [ -f /etc/nginx/templates/default.conf.template ]; then
     envsubst '${NGINX_BASE_DOMAIN} ${NGINX_SERVER_PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-    
-    API_CERT="/etc/letsencrypt/live/api.${NGINX_BASE_DOMAIN}/fullchain.pem"
-    ADMIN_CERT="/etc/letsencrypt/live/admin.${NGINX_BASE_DOMAIN}/fullchain.pem"
-    
-    if [ ! -f "$API_CERT" ] || [ ! -f "$ADMIN_CERT" ]; then
-        echo "Warning: SSL certificates not found. Removing SSL server blocks..."
+fi
+
+API_CERT="/etc/letsencrypt/live/api.${NGINX_BASE_DOMAIN}/fullchain.pem"
+ADMIN_CERT="/etc/letsencrypt/live/admin.${NGINX_BASE_DOMAIN}/fullchain.pem"
+
+if [ -f "$API_CERT" ] && [ -f "$ADMIN_CERT" ]; then
+    if [ -f /etc/nginx/templates/ssl.conf.template ]; then
+        echo "SSL certificates found. Generating SSL configuration and enabling HTTPS redirects..."
+        envsubst '${NGINX_BASE_DOMAIN} ${NGINX_SERVER_PORT}' < /etc/nginx/templates/ssl.conf.template > /etc/nginx/conf.d/ssl.conf
+        
         awk '
-        BEGIN { in_ssl_block = 0; brace_count = 0 }
-        /^server {$/ {
-            if (in_ssl_block == 0) {
-                print
-                getline
-                if (/listen 443 ssl/) {
-                    in_ssl_block = 1
-                    brace_count = 1
-                    next
-                } else {
+        BEGIN { skip_api = 0; skip_admin = 0; api_braces = 0; admin_braces = 0 }
+        /# HTTP_REDIRECT_PLACEHOLDER/ {
+            skip_api = 1
+            api_braces = 1
+            print "        return 301 https://$host$request_uri;"
+            next
+        }
+        skip_api == 1 {
+            if (/\{/) api_braces++
+            if (/\}/) {
+                api_braces--
+                if (api_braces == 0) {
+                    skip_api = 0
                     print
                 }
             }
             next
         }
-        in_ssl_block == 1 {
-            if (/\{/) brace_count++
+        /# ADMIN_HTTP_REDIRECT_PLACEHOLDER/ {
+            skip_admin = 1
+            admin_braces = 1
+            print "        return 301 https://$host$request_uri;"
+            next
+        }
+        skip_admin == 1 {
+            if (/\{/) admin_braces++
             if (/\}/) {
-                brace_count--
-                if (brace_count == 0) {
-                    in_ssl_block = 0
+                admin_braces--
+                if (admin_braces == 0) {
+                    skip_admin = 0
+                    print
                 }
             }
             next
         }
         { print }
-        ' /etc/nginx/conf.d/default.conf > /tmp/nginx.conf.tmp && mv /tmp/nginx.conf.tmp /etc/nginx/conf.d/default.conf
-        echo "SSL server blocks removed. Nginx will work in HTTP-only mode until certificates are obtained."
+        ' /etc/nginx/conf.d/default.conf > /tmp/nginx.conf.tmp
+        
+        if [ $? -eq 0 ]; then
+            mv /tmp/nginx.conf.tmp /etc/nginx/conf.d/default.conf
+            echo "" >> /etc/nginx/conf.d/default.conf
+            echo "include /etc/nginx/conf.d/ssl.conf;" >> /etc/nginx/conf.d/default.conf
+            echo "SSL configuration generated and HTTPS redirects enabled."
+        else
+            echo "Error processing config"
+        fi
     fi
+else
+    echo "SSL certificates not found. Removing SSL configuration if exists..."
+    rm -f /etc/nginx/conf.d/ssl.conf
+    sed -i '/include \/etc\/nginx\/conf.d\/ssl.conf;/d' /etc/nginx/conf.d/default.conf
+    sed -i 's|# HTTP_REDIRECT_PLACEHOLDER||' /etc/nginx/conf.d/default.conf
+    sed -i 's|# ADMIN_HTTP_REDIRECT_PLACEHOLDER||' /etc/nginx/conf.d/default.conf
+    echo "Nginx will work in HTTP-only mode until certificates are obtained."
 fi
 
 if [ -f /docker-entrypoint.sh ]; then
